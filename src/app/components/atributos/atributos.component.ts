73608d336f6659d56f4c25dad2e19190b244c74e
João Carlos <joao.carlos_25@hotmail.com>
2023-09-04 22:02:33 -0300

    ngStyle

--- src/app/components/atributos/atributos.component.ts
+++ src/app/components/atributos/atributos.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class AtributosComponent {
 
   estilo: string = "disable";
+  corFundo: string = "blue";
+  corFonte: string = "white";
 
   trocar(){
     if(this.estilo == "disable"){
